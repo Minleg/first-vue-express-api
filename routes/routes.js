7a5b980dbc12d01f2d31c8ff7178c89a80375870
minleg <minleg@minleg.com>
2022-04-06 19:57:05 -0500

    update api message

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ let router = express.Router() // creates a router object  which understands how 
 
 // configure a route that is a get request, a request that fetches data and path is /, the base request path
 router.get('/', function(req, res, next){  // function will run to provide a response. arguments req-request which is the request that the client has made on the browser , res-response is the reponse that server is gonna send back for that request and next
-    res.json( {  'message': 'Hello ITEC 2560 Web Programmers' }) // sends a json response back.  we can response html web pages, binary files or images etc
+    res.json( {  'message': 'Hello AWESOME ITEC 2560 Web Programmers' }) // sends a json response back.  we can response html web pages, binary files or images etc
 })
 
 module.exports = router // another file will be able to include or require this file and it will be able to access this router
